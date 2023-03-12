3f25e46c91e59da6aac56bdcd9986adfe14ecd61
Sarah Baughman <sarahbaughman92@gmail.com>
2023-03-12 14:51:30 -0400

    commit tree a7680e8e60facf4a99d25f9a28ead18bfd25c8b2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(name,event) {
+  let cards = []
+  for (let i = 0; i < name.length; i++) {
+    cards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+return cards
+}
+
+function countDown(num) {
+  while (num >= 0)
+  console.log(num--)
+}
